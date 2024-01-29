50e9f7a50ea29b509bb23a7b113610143fc7a24c
Pankaj-Thorat <thoratpankaj282@gmail.com>
2024-01-29 19:29:44 +0530

    [Pankaj] Changes Made in Edit [Profile img]

---
+++ EmployeePayRollMVC/wwwroot/js/custom.js
@@ -0,0 +1,18 @@
+﻿
+
+
+
+
+function validateDate() {
+    var appointmentDataInput = document.getElementById("startDate");
+    var selectedDate = new Date(appointmentDataInput.value);
+
+    var currentDate = new Date();
+
+    currentDate.setHours(0, 0, 0, 0);
+
+    if (selectedDate < currentDate) {
+        alert('Please select future date!!');
+        appointmentDataInput.value = '';
+    }
+}
